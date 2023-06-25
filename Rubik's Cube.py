8aed355b7ad568c8d668d6414376dd61368d1eef
Aditya Sengupta <adityasngpta@gmail.com>
2023-06-25 15:11:12 -0700

    Add files via upload

--- Rubik's Cube.py
+++ Rubik's Cube.py
@@ -8,175 +8,106 @@ rubiks_cube = np.array([[[0, 0, 0],
                         [0, 0, 0]],
     
                         [[1, 1, 1],
-                        [1, 1, 1],  # White
+                        [1, 1, 1],  # Orange
                         [1, 1, 1]],
 
                         [[2, 2, 2],
-                        [2, 2, 2],  # Orange
+                        [2, 2, 2],  # Red
                         [2, 2, 2]],
 
                         [[3, 3, 3],
-                        [3, 3, 3], # Red
+                        [3, 3, 3], # Blue
                         [3, 3, 3]],
                         
                         [[4, 4, 4],
-                        [4, 4, 4], # Blue
+                        [4, 4, 4], # Green
                         [4, 4, 4]],
-
+                        
                         [[5, 5, 5],
-                        [5, 5, 5], # Green
-                        [5, 5, 5]]])
+                         [5, 5, 5], # White
+                         [5, 5, 5]]])
+
+rubiks_cube = np.array([[[4, 3, 4],
+                        [2, 0, 0],  
+                        [0, 0, 0]],
+    
+                        [[5, 2, 5],
+                        [1, 1, 5],  
+                        [2, 3, 2]],
+
+                        [[3, 1, 2],
+                        [0, 2, 1],  
+                        [1, 5, 4]],
+
+                        [[1, 4, 1],
+                        [3, 3, 4], 
+                        [5, 2, 5]],
+                        
+                        [[4, 2, 2],
+                        [3, 4, 5], 
+                        [0, 1, 0]],
+
+                        [[3, 4, 1],
+                         [5, 5, 4],
+                         [3, 0, 3]]])
 
 
 def rotate_face(direction, cube):
     '''
     (spin face)
-    s_1
-    s_2
-    s_3
+    s (clockwise)
     
     (bottom)
-    b_1
-    b_2
-    b_3
+    b (clockwise)
 
     ^ Rotating right ^
 
     (right)
-    r_1
-    r_2
-    r_3
+    r
     
     (left)
-    l_1
-    l_2
-    l_3
+    l
 
     ^ Rotating up ^
     '''
-
-    if direction == "b_1":
-        cache = copy.copy(cube[0,2])
-        cube[0,2] = cube[4,2]
-        cube[4,2] = cube[1,2]
-        cube[1,2] = cube[5,2]
-        cube[5,2] = cache
-
-    if direction == "b_2":
-        cache = cube[0,2]
-        cube[0,2] = cube[2,2]
-        cube[2,2] = cache
-        cube[4,2] = cube[5,2]
-        cube[5,2] = cube[4,2]
-
-    if direction == "b_3":
-        cache = cube[0,2]
-        cube[0,2] = cube[5,2]
-        cube[5,2] = cube[2,2]
-        cube[2,2] = cube[4,2]
-        cube[4,2] = cache
-
-    if direction == "s_1":
-        cube[0] = np.rot90(cube[0])
-        cache = cube[3,0]
-
-        cube[5,0,0] = cache[0]
-        cube[5,1,0] = cache[1]
-        cube[5,2,0] = cache[2]
+    if direction == "s":
+        cube[0] = np.rot90(cube[0], 3)
+        cache = copy.copy(cube[2, 0])
+        cube[2, 0] = cube[4, 0]
+        cube[4, 0] = cube[1, 0]
+        cube[1, 0] = cube[3, 0]
+        cube[3, 0] = cache
         
-        cube[2,2] = [cube[5,0,0], cube[5,1,0], cube[5,2,0]]
+    elif direction == "b":
+        cube[2] = np.rot90(cube[2], 3)
+        cache1 = copy.copy(cube[0, 2, 0])
+        cache2 = copy.copy(cube[0, 2, 1])
+        cache3 = copy.copy(cube[0, 2, 2])
 
-        cube[4,0,2] = cube[2,2,0]
-        cube[4,1,2] = cache[2,2,1]
-        cube[4,2,2] = cache[2,2,2]
+        cube[0, 2, 0] = cube[3, 2, 2]
+        cube[0, 2, 1] = cube[3, 1, 2]
+        cube[0, 2, 2] = cube[3, 0, 2]
 
-        cube[3,0] = [cube[4,0,2], cube[4,1,2], cube[4,2,2]]
+        cube[3, 2, 2] = cube[5, 2, 2]
+        cube[3, 1, 2] = cube[5, 1, 2]
+        cube[3, 0, 2] = cube[5, 0, 2]
 
-    if direction == "s_2":
-        cube[0] = np.rot90(cube[0])
-        cube[0] = np.rot90(cube[0])
-        cache = cube[3,0]
+        cube[5, 2, 2] = cube[3, 0, 0]
+        cube[5, 1, 2] = cube[3, 1, 0]
+        cube[5, 0, 2] = cube[3, 2, 0]
 
-        cache1 = cube[5,0,0]
-        cache2 = cube[5,1,0] 
-        cache3 = cube[5,2,0]
+        cube[3, 0, 0] = cache1
+        cube[3, 1, 0] = cache2
+        cube[3, 2, 0] = cache3   
 
-        cube[5,0,0] = cube[4,2,2]
-        cube[5,1,0] = cube[4,1,2]
-        cube[5,2,0] = cube[4,0,2]
         
-        cube[3,0] = cube[2,2]
-
-        cube[2,2] = cache
-
-        cube[4,2,2] = cache1
-        cube[4,1,2] = cache2
-        cube[4,0,2] = cache3
-
-    if direction == "s_3":
-        cube[0] = np.rot90(cube[0])
-        cube[0] = np.rot90(cube[0])
-        cube[0] = np.rot90(cube[0])
-        cache = cube[3,0]
-
-        cube[5,0,0] = cube[2,2,0]
-        cube[5,1,0] = cache[2,2,1]
-        cube[5,2,0] = cache[2,2,2]
-        
-        cube[2,2] = [cube[4,0,2], cube[4,1,2], cube[4,2,2]]
-
-        cube[4,0,2] = cache[0]
-        cube[4,1,2] = cache[1]
-        cube[4,2,2] = cache[2]
-
-        cube[3,0] = [cube[5,0,0], cube[5,1,0], cube[5,2,0]]
-
-    if direction == "r_1":
-        cache = cube[1,2]
-        cube[1,2] = cube[4,2]
-        cube[4,2] = cube[3,2]
-        cube[3,2] = cube[5,2]
-        cube[5,2] = cache
-
-    if direction == "r_2":
-        cache = cube[1,2]
-        cube[1,2] = cube[3,2]
-        cube[3,2] = cache
-        cube[4,2] = cube[5,2]
-        cube[5,2] = cube[4,2]
-
-    if direction == "r_3":
-        cache = cube[1,2]
-        cube[1,2] = cube[5,2]
-        cube[5,2] = cube[3,2]
-        cube[3,2] = cube[4,2]
-        cube[4,2] = cache
-
-    if direction == "l_1":
-        cache = cube[1,0]
-        cube[1,0] = cube[5,0]
-        cube[5,0] = cube[3,0]
-        cube[3,0] = cube[4,0]
-        cube[4,0] = cache
-
-    if direction == "l_2":
-        cache = cube[1,0]
-        cube[1,0] = cube[3,0]
-        cube[3,0] = cache
-        cube[4,0] = cube[5,0]
-        cube[5,0] = cube[4,0]
-
-    if direction == "l_3":
-        cache = cube[1,0]
-        cube[1,0] = cube[4,0]
-        cube[4,0] = cube[3,0]
-        cube[3,0] = cube[5,0]
-        cube[5,0] = cache
-
     return cube
 
-    
-print(rotate_face("b_1", rubiks_cube))
+output = (rotate_face("none", rubiks_cube))
+
+while True:
+    output = (rotate_face(str(input()), output))
+    print(output)
 
     
 
