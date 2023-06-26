d6ddd4739d38d31a0ae5cfb75c678b3297ad0686
Aditya Sengupta <adityasngpta@gmail.com>
2023-06-25 17:10:52 -0700

    Add files via upload

--- Rubik's Cube.py
+++ Rubik's Cube.py
@@ -3,73 +3,52 @@ import copy
 
 # front, back, top, bottom, right, left
 
-rubiks_cube = np.array([[[0, 0, 0],
-                        [0, 0, 0],  # Yellow
-                        [0, 0, 0]],
+rubiks_cube = np.array([[[0, 0], # White
+                        [0, 0]],
     
-                        [[1, 1, 1],
-                        [1, 1, 1],  # Orange
-                        [1, 1, 1]],
+                        [[1, 1], # Blue
+                        [1, 1]],
 
-                        [[2, 2, 2],
-                        [2, 2, 2],  # Red
-                        [2, 2, 2]],
+                        [[2, 2], # Green
+                        [2, 2]],
 
-                        [[3, 3, 3],
-                        [3, 3, 3], # Blue
-                        [3, 3, 3]],
+                        [[3, 3], # Orange
+                        [3, 3]],
                         
-                        [[4, 4, 4],
-                        [4, 4, 4], # Green
-                        [4, 4, 4]],
+                        [[4, 4], # Red
+                        [4, 4]],
                         
-                        [[5, 5, 5],
-                         [5, 5, 5], # White
-                         [5, 5, 5]]])
+                        [[5, 5], # Yellow
+                         [5, 5]]])
 
-rubiks_cube = np.array([[[4, 3, 4],
-                        [2, 0, 0],  
-                        [0, 0, 0]],
+rubiks_cube = np.array([[[0, 2],
+                        [1, 2]],
     
-                        [[5, 2, 5],
-                        [1, 1, 5],  
-                        [2, 3, 2]],
+                        [[4, 1],
+                        [5, 4]],
 
-                        [[3, 1, 2],
-                        [0, 2, 1],  
-                        [1, 5, 4]],
+                        [[5, 3],
+                        [1, 3]],
 
-                        [[1, 4, 1],
-                        [3, 3, 4], 
-                        [5, 2, 5]],
+                        [[3, 5],
+                        [2, 0]],
                         
-                        [[4, 2, 2],
-                        [3, 4, 5], 
-                        [0, 1, 0]],
-
-                        [[3, 4, 1],
-                         [5, 5, 4],
-                         [3, 0, 3]]])
+                        [[5, 5],
+                        [0, 3]],
+                        
+                        [[4, 2],
+                         [0, 1]]])
 
 
 def rotate_face(direction, cube):
     '''
-    (spin face)
-    s (clockwise)
+    up (u) (clockwise)
     
-    (bottom)
-    b (clockwise)
-
-    ^ Rotating right ^
-
-    (right)
-    r
+    right (r) (clockwise)
     
-    (left)
-    l
-
-    ^ Rotating up ^
+    spin (s) (clockwise)
     '''
+
     if direction == "s":
         cube[0] = np.rot90(cube[0], 3)
         cache = copy.copy(cube[2, 0])
@@ -77,37 +56,60 @@ def rotate_face(direction, cube):
         cube[4, 0] = cube[1, 0]
         cube[1, 0] = cube[3, 0]
         cube[3, 0] = cache
-        
-    elif direction == "b":
-        cube[2] = np.rot90(cube[2], 3)
-        cache1 = copy.copy(cube[0, 2, 0])
-        cache2 = copy.copy(cube[0, 2, 1])
-        cache3 = copy.copy(cube[0, 2, 2])
 
-        cube[0, 2, 0] = cube[3, 2, 2]
-        cube[0, 2, 1] = cube[3, 1, 2]
-        cube[0, 2, 2] = cube[3, 0, 2]
+    elif direction == "r":
+        cube[1] = np.rot90(cube[1], 3)
+        cache0 = copy.copy(cube[0, 1, 0])
+        cache1 = copy.copy(cube[0, 1, 1])
+        cube[0, 1, 0] = cube[3, 1, 0]
+        cube[0, 1, 1] = cube[3, 1, 1]
+        cube[3, 1, 0] = cube[5, 1, 0]
+        cube[3, 1, 1] = cube[5, 1, 1]
+        cube[5, 1, 0] = cube[4, 1, 0]
+        cube[5, 1, 1] = cube[4, 1, 1]
+        cube[4, 1, 0] = cache0
+        cube[4, 1, 1] = cache1
 
-        cube[3, 2, 2] = cube[5, 2, 2]
-        cube[3, 1, 2] = cube[5, 1, 2]
-        cube[3, 0, 2] = cube[5, 0, 2]
 
-        cube[5, 2, 2] = cube[3, 0, 0]
-        cube[5, 1, 2] = cube[3, 1, 0]
-        cube[5, 0, 2] = cube[3, 2, 0]
-
-        cube[3, 0, 0] = cache1
-        cube[3, 1, 0] = cache2
-        cube[3, 2, 0] = cache3   
-
+    elif direction == "u":
+        cube[4] = np.rot90(cube[4], 3)
+        cache0 = copy.copy(cube[0, 0, 1])
+        cache1 = copy.copy(cube[0, 1, 1])
+        cube[0, 0, 1] = cube[2, 0, 1]
+        cube[0, 1, 1] = cube[2, 1, 1]
+        cube[2, 0, 1] = cube[5, 0, 1] 
+        cube[2, 1, 1] = cube[5, 1, 1]
+        cube[5, 0, 1] = cube[1, 0, 1]
+        cube[5, 1, 1] = cube[1, 1, 1]
+        cube[1, 0, 1] = cache0
+        cube[1, 1, 1] = cache1
         
     return cube
 
 output = (rotate_face("none", rubiks_cube))
 
+X = output - output
+Y = np.array([])
+
 while True:
-    output = (rotate_face(str(input()), output))
+    X += output
+    y = str(input())
+
+    if y == "s":
+        Y += np.array([3])
+
+    elif y == "r":
+        Y += np.array([1])
+
+    elif y == "u":
+        Y += np.array([2])
+
+    output = (rotate_face(y, output))
+    np.save('X.npy', X)
+    np.save('Y.npy', Y)
     print(output)
+    
+
 
     
 
